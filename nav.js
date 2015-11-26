3c03813a59b41b725a7bbe9e65ad07b977fb1a37
Ayco <ayco@live.nl>
2015-11-26 15:23:22 +0100

    Opdracht 1 tm 8

---
+++ nav.js
@@ -0,0 +1,8 @@
+  var str ='<nav class="navbar navbar-default">    <div class="navbar-header">	<ul class="nav nav-pills">';
+      str+=' <li><a href="index.html">Home</a></li>';
+      str+=' <li><a href="voorstellen.html">Voorstellen</a></li>';
+      str+=' <li><a href="contact.html">Contact</a></li>';
+      str+=' <li><a href="dummy.html">Dummypagina</a></li>';
+      str+='</ul>	</div>	</nav>';
+document.write(str);
+// Add other links a desired
